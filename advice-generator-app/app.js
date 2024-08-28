991f4f6573eb2dece851ab49f6e5c22b8311e3b7
Anneloes Oude Vrielink <a.s.oude.vrielink@gmail.com>
2024-08-28 14:19:56 +0200

    js functionality added

--- advice-generator-app/app.js
+++ advice-generator-app/app.js
@@ -1,11 +1,30 @@
-// upon button click - display random number
-function displayMessage() {
-    const randomNumber = Math.floor(Math.random()*100);
+const API = "https://api.adviceslip.com/advice";
 
-    const numberDisplay = document.getElementById('advice');
-    numberDisplay.textContent = `The number is: ${randomNumber}`
+// Function to display the advice
+async function displayAdvice() {
+    const advice = await getAdvice();
+    console.log(`advice obtained: ${advice.advice}`);
+    const display = document.getElementById('advice');
+    display.textContent = `"${advice.advice}"`;
+    const displayHeader = document.getElementById('advice-header');
+    displayHeader.textContent = `ADVICE #${advice.id}`;
 }
 
+// Function to get advice from the API using async/await
+async function getAdvice() {
+    try {
+        const response = await fetch(API);
+        if (!response.ok) {
+            throw new Error('Request failed!');
+        }
+        const jsonResponse = await response.json();
+        return jsonResponse.slip; 
+    } catch (error) {
+        console.error('Error fetching advice:', error);
+        return { id: null, advice: 'Failed to fetch advice' };
+    }
+}
+
+displayAdvice();
 const button = document.getElementById('icon-box');
-console.log(button);
-button.addEventListener('click', displayMessage);
+button.addEventListener('click', displayAdvice);
